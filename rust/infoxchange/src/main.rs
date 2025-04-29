8d4db4bf51fc1fa5a4c66fb76d64048a06240433
Sean Bentley <sfb103@gmail.com>
2025-04-29 00:02:59 +0000

    testing

--- rust/infoxchange/src/main.rs
+++ rust/infoxchange/src/main.rs
@@ -1,12 +1,19 @@
 use std::sync::{Arc, Mutex};
-use std::thread;
+
+use wasmtime::{component::*, Config, Error};
+use wasmtime::{Result, Engine, Store};
+use wasmtime_wasi::{IoView, WasiCtx, WasiView};
+
+use async_executor::executor::Executor;
+use async_executor::tokio_executor::TokioExecutor;
 
 use wasmexample::infoxchange::status_holder::{self, Status};
-use wasmtime::{component::*, Error};
-use wasmtime::{Result, Engine, Store};
-use wasmtime_wasi::{WasiCtx, WasiView};
 
-bindgen!("infoxchange" in "./wit");
+bindgen!({
+    world: "infoxchange",
+    path: "./wit",
+    async: true,
+});
 
 #[derive(Clone)]
 struct CompContext {
@@ -15,15 +22,18 @@ struct CompContext {
 }
 
 impl CompContext {
-    pub fn new(wasm: &str) -> Result<Self, Error> {
-        // create our component
-        let engine = Engine::default();
-        let component = Component::from_file(&engine, wasm)?;
-    
+    pub async fn new(wasm: &str) -> Result<Self, Error> {
+        // setup our engine and linker to support an async context
+        let mut config = Config::new();
+        config.async_support(true);
+        let engine = Engine::new(&config).unwrap();    
         let mut linker = Linker::<XchangeHost>::new(&engine);
-        wasmtime_wasi::add_to_linker_sync(&mut linker)?;
+
+        // add our host to the linker, so we can provide a world for our component
+        wasmtime_wasi::add_to_linker_async(&mut linker)?;
         Infoxchange::add_to_linker(&mut linker, |s: &mut XchangeHost| s)?;
     
+        // setup our store that holds the context our host uses with our component
         let mut store = Store::new(
             &engine,
             XchangeHost { 
@@ -34,8 +44,12 @@ impl CompContext {
                 status: Status::Unknown
             },
         );
-        let bindings = Infoxchange::instantiate(&mut store, &component, &linker)?;
-        
+
+        // instantiate our component
+        let component = Component::from_file(&engine, wasm).unwrap();
+        let instance = linker.instantiate_async(&mut store, &component).await?;
+        let bindings = Infoxchange::new(&mut store, &instance)?;
+
         Ok(Self {
             store: Arc::new(Mutex::new(store)),
             bindings: Arc::new(bindings),
@@ -49,52 +63,63 @@ struct XchangeHost {
     status: Status,
 }
 
-impl WasiView for XchangeHost {
+impl IoView for XchangeHost{
     fn table(&mut self) -> &mut ResourceTable {
         &mut self.table
     }
+}
+impl WasiView for XchangeHost {
     fn ctx(&mut self) -> &mut WasiCtx {
         &mut self.wasi_ctx
     }
 }
 
 impl status_holder::Host for XchangeHost {
-    fn set_status(&mut self,status: Status) -> Result<(),String> {
+    async fn set_status(&mut self,status: Status) -> Result<(),String> {
         println!("set_status() called with status: {:?}", status);
         self.status = status;
         Ok(())
     }
 
-    fn get_status(&mut self,) -> Result<Status,String> {
+    async fn get_status(&mut self,) -> Result<Status,String> {
         Ok(self.status)
     }
 }
 
-fn comp_worker(comp_ctx: CompContext) -> Result<()> {
+async fn comp_worker<E: Executor>(executor: E, comp_ctx: CompContext) -> Result<()> {
     let comp = comp_ctx.bindings.wasmexample_infoxchange_worker();
 
     // call the components's do_work(), note this gives the component a thread to execute on 
     loop {
-        println!("\ncomp_worker() calling do_work()");
-        let mut store = comp_ctx.store.lock().unwrap();
-        if !comp.call_do_work(&mut *store)? {
-            break; // if do_work returns false, the break out of this loop and exit the thread
+        // These braces will ensure our mutex gets dropped before we enter the .await
+        // This is something that could aslo be accomplished by std::mem::drop, but
+        //    doing a drop just before the .await seems to confuse rust's static analysis...
+        //    so using the braces to accomplish the same thing.
+        { 
+            println!("\ncomp_worker() calling do_work()");
+            let mut store = comp_ctx.store.lock().unwrap();
+            
+            if !comp.call_do_work(&mut *store).await? {
+            //if !wasmtime_wasi::runtime::with_ambient_tokio_runtime(||comp.call_do_work(&mut *store))? {
+                break; // if do_work returns false, the break out of this loop and exit the thread
+            }
         }
-        std::mem::drop(store);
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        executor.sleep(std::time::Duration::from_secs(1)).await;
     }
 
     Ok(())
 }
 
-fn main() -> Result<()> {
-    let comp_ctx = CompContext::new("./infoxchange.wasm")?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let executor = TokioExecutor;
+    let comp_ctx = CompContext::new("./infoxchange.wasm").await?;
     let worker_ctx = comp_ctx.clone();
-    let comp_worker = thread::spawn( move|| comp_worker(worker_ctx).unwrap() );
+    let comp_worker = executor.spawn( comp_worker(executor,worker_ctx) );
     
     // just for this test case, let's sleep for 3s to give our comp_worker a chance to run for a bit before
     // setting the id
-    std::thread::sleep(std::time::Duration::from_secs(3));
+    executor.sleep(std::time::Duration::from_secs(3)).await;
 
     {
         let id = 1;
@@ -106,6 +131,6 @@ fn main() -> Result<()> {
         };
     }
 
-    comp_worker.join().unwrap();
+    comp_worker.await.unwrap()?;
     Ok(())
 }
